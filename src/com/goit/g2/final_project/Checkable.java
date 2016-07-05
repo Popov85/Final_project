a1d36d81f0a29f7d96dba28d57c416808654534c
Andrii Popov <andrey.popov85@gmail.com>
2016-07-05 13:29:35 +0300

    Documentation has become more clear and specific

--- src/com/goit/g2/final_project/Checkable.java
+++ src/com/goit/g2/final_project/Checkable.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
  * Interface {@code Checkable}  for validating a credit card number in two steps:
  * 1. General input errors;
  * 2. Luhn algorithm;
- * @author G2 team at GoJavaOnline#3:
+ * @author G2 team at GoJavaOnline#3 including:
  * @author Igor Stolyarov
  * @author Serhii Toporko
  * @author Roman Kalyga
@@ -16,11 +16,11 @@ public interface Checkable {
 
         /**
          * Checks for general mistakes in input:
-         * 1. letters instead of numbers;
+         * 1. Letters instead of/mixed with numbers;
          * 2. Quantity of letters more or less 16;
          * 3. Ignores spaces between number groups if present.
          * @return true if what a user entered is a plausible credit card number, false otherwise
-         * @throws InputMismatchException
+         * @throws InputMismatchException if nothing (empty string) was entered by a user
          */
         boolean isInputValid() throws InputMismatchException;
 
